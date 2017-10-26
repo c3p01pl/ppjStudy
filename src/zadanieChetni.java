983bf8408e67b2d71154c4a44a0a997687bd3b26
c3p01pl <32600271+c3p01pl@users.noreply.github.com>
2017-10-26 12:37:44 +0200

    Update zadanieChetni.java
    change checker metod

--- src/zadanieChetni.java
+++ src/zadanieChetni.java
@@ -1,60 +1,70 @@
-public class zadanieChetni {
-    public static void main(String [] args){
+import java.util.Arrays;
+
+
+public class wilingEx {
+    /*
+     * Checking if chars are proper according to chosen numeral system.
+     */
+    public static void checker(String rz1, String rz0, String[] znakiDop){
+
+
+        boolean flag1 = Arrays.asList(znakiDop).contains(rz1);
+        boolean flag0 = Arrays.asList(znakiDop).contains(rz0);
+
+        if (!(flag1 & flag0)){
+            System.out.println("Wprowadzono złe dane.");
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args){
+
+//        char[] znaki = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','a','b','c','d','e','f'};
+        String[] znaki = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","a","b","c","d","e","f"};
+
+
         java.util.Scanner in = new java.util.Scanner(System.in);
-        System.out.print("Podaj char b lub d lub h:");
+        System.out.print("Podaj typ liczby (b - binarny, d - dziesietny, h - szesnastkowy): ");
         char typ = in.next().charAt(0);
+
+        System.out.print("Podaj rzad1: ");
+        char rzad1 = in.next().charAt(0);
+        System.out.print("Podaj rzad0: ");
+        char rzad0 = in.next().charAt(0);
+        String LiczbaRzad1 = Character.toString(rzad1);
+        String LiczbaRzad0 = Character.toString(rzad0);
+        String liczbaString = LiczbaRzad1 + LiczbaRzad0;
+
         switch (typ) {
             case 'b':
-                System.out.println("binary"); break;
+                System.out.println("Wybrano System Binarny.");
+                znaki = Arrays.copyOfRange(znaki, 0, 2);
+                checker(LiczbaRzad1,LiczbaRzad0,znaki);
+                int wynik = Integer.parseInt(liczbaString,2);
+                System.out.println("Twoja liczba w systemie binarnym to: " + liczbaString);
+                System.out.println("Twoja liczba w systemie dziesiętnym to: " + wynik);
+                System.out.println("Twoja liczba w systemie szesnastkowym to: " + wynik);
+                break;
             case 'd':
-                System.out.println("decimal"); break;
+                System.out.println("Wybrano System Dziesiętny.");
+                znaki = Arrays.copyOfRange(znaki, 0, 11);
+                checker(LiczbaRzad1,LiczbaRzad0,znaki);
+                wynik = Integer.parseInt(liczbaString);
+                System.out.println("Twoja liczba w systemie binarnym to: " + Integer.toBinaryString(wynik));
+                System.out.println("Twoja liczba w systemie dziesiętnym to: " + wynik);
+                System.out.println("Twoja liczba w systemie szesnastkowym to: " + Integer.toHexString(wynik).toUpperCase());
+                break;
             case 'h':
-                System.out.println("hexadecimal"); break;
+                System.out.println("Wybrano System szesnastkowy.");
+                checker(LiczbaRzad1,LiczbaRzad0,znaki);
+                wynik = Integer.parseInt(liczbaString,16);
+                System.out.println("Twoja liczba w systemie binarnym to: " + Integer.toBinaryString(wynik));
+                System.out.println("Twoja liczba w systemie dziesiętnym to: " + wynik);
+                System.out.println("Twoja liczba w systemie szesnastkowym to: " + liczbaString);
+                break;
             default:
-                System.out.println("ERROR"); break;
-        }
-        char rzad1,rzad0;
-        System.out.print("Podaj rzad1, rzad0: ");
-        rzad1 = in.next().charAt(0);
-        rzad0 = in.next().charAt(0);
-        char[] znaki = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','a','b','c','d','e','f'};
-        System.out.println(znaki[15]);
-        switch (typ){
-            case 'b':
-                if ( (rzad1 == '0' |  rzad1 == '1') & (rzad0 == '1') | (rzad0 == '0') ) {
-                System.out.println("jest ok");
-                }
-                else { System.out.println("Wprowadzono zle dane"); }
-                break;
-            case 'd':
-                for (int i=10; i<16; i++){
-                    if (rzad1 == znaki[i] | rzad0 == znaki[i]){
-                        System.out.println("Wprowadzono zle dane");
-                    }
-                }
-                break;
-            case 'h':
-                boolean f0 = false;
-                boolean f1 = false;
-                for (int i=0; i<16; i++) {
-                    if (rzad1 == znaki[i]) {
-                        f0 = true;
-                    }
-                }
-                if (f0){
-                    for (int i=0; i<16; i++) {
-                        if (rzad0 == znaki[i]) {
-                            f1 = true;
-                        }
-                    }
-                    if (f1 == false){
-                        System.out.println("Wprowdzono zle dane r1");
-
-                    }
-                }
-                    else { System.out.println("Wprowdzono zle dane r0"); }
-                break;
-
+                System.out.println("ERROR");
+                System.exit(1);
         }
     }
 }
