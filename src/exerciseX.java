772b01f90a7b6febd5956bdb415f1ee0857cdb25
cep01pl <m.trochimiu2@gmail.com>
2017-11-01 22:35:40 +0100

    new better program. I hope....

---
+++ src/exerciseX.java
@@ -0,0 +1,110 @@
+import java.util.Scanner;
+
+public class exerciseX{
+
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Podaj typ liczby (b - binarny, d - dziesietny, h - szesnastkowy): ");
+        char typ = in.next().charAt(0);
+
+        System.out.print("Podaj rzad1: ");
+        char rzad1 = in.next().charAt(0);
+        rzad1 = Character.toUpperCase(rzad1);
+
+        System.out.print("Podaj rzad0: ");
+        char rzad0 = in.next().charAt(0);
+        rzad0 = Character.toUpperCase(rzad0);
+
+
+        switch (typ) {
+            case 'b':
+                System.out.println("Wybrano System Binarny.");
+                if (checkIfCharsBelongToNumericalSystem(rzad1,rzad0,'1')){
+
+                    int wynikDz = convertToDecimal(rzad1,rzad0,2);
+
+                    System.out.println("Twoja liczba w systemie binarnym to: " + rzad1 + rzad0);
+                    System.out.println("Twoja liczba w systemie dziesiętnym to: " + wynikDz);
+                    System.out.println("Twoja liczba w systemie szesnastkowym to: " + wynikDz);
+                }
+                break;
+            case 'd':
+                System.out.println("Wybrano System Dziesiętny.");
+                if (checkIfCharsBelongToNumericalSystem(rzad1,rzad0,'9')){
+
+                    String liczbaString = Character.toString(rzad1) + Character.toString(rzad0);
+                    int wartDec = Integer.parseInt(liczbaString);
+
+                    String wynikBin = convertDecimalToHexOrBin(wartDec,2);
+                    String wynikHex = convertDecimalToHexOrBin(wartDec,16);
+
+                    System.out.println("Twoja liczba w systemie binarnym to: " + wynikBin);
+                    System.out.println("Twoja liczba w systemie dziesiętnym to: " + rzad1 + rzad0);
+                    System.out.println("Twoja liczba w systemie szesnastkowym to: " + wynikHex);
+                }
+                break;
+            case 'h':
+                System.out.println("Wybrano System szesnastkowy.");
+                if (checkIfCharsBelongToNumericalSystem(rzad1,rzad0,'F')){
+
+                    int wynikDz = convertToDecimal(rzad1,rzad0,16);
+                    String wynikBin = convertDecimalToHexOrBin(wynikDz,2);
+
+                    System.out.println("Twoja liczba w systemie binarnym to: " + wynikBin);
+                    System.out.println("Twoja liczba w systemie dziesiętnym to: " + wynikDz);
+                    System.out.println("Twoja liczba w systemie szesnastkowym to: " + rzad1 + rzad0);
+                }
+                break;
+            default:
+                System.out.println("ERROR");
+                System.exit(1);
+
+
+        }
+    }
+
+
+    public static boolean checkIfCharsBelongToNumericalSystem(char rz1, char rz0, char lastChar){
+
+            boolean ok = false;
+
+            if (!(rz1 > lastChar | rz0 > lastChar)) {
+                ok = true;
+            }
+            else {System.out.println("Wprowadzono złe dane.");}
+
+            return ok;
+    }
+
+    public static int convertToDecimal(char rzad1, char rzad0, int system){
+        String strliczba1 = Character.toString(rzad1);
+        String strliczba0 = Character.toString(rzad0);
+        int liczba0;
+        int liczba1;
+
+        if (system == 16){
+            String digits = "0123456789ABCDEF";
+            liczba0  = digits.indexOf(strliczba0);
+            liczba1  = digits.indexOf(strliczba1);
+        }
+        else{
+            liczba1 = Integer.parseInt(strliczba1);
+            liczba0 = Integer.parseInt(strliczba0);
+        }
+        int wynik = liczba0+system*liczba1;
+        return wynik;
+    }
+
+    public static String convertDecimalToHexOrBin(int wartDec, int system){
+        String wartHex = "";
+        String[] HexStr = {"0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"};
+        while(wartDec>0)
+        {
+            int reszta=wartDec%system;
+            wartHex=HexStr[reszta]+ wartHex;
+            wartDec=wartDec/system;
+        }
+        return wartHex;
+    }
+}
