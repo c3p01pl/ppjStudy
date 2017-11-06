d73a30957b4d6b5abf31109c02105d4960a0acff
cep01pl <m.trochimiu2@gmail.com>
2017-11-07 00:01:21 +0100

    add inputing rows and colmuns and checking if operation is impossible.

--- src/sumOfDiagonalElements.java
+++ src/sumOfDiagonalElements.java
@@ -1,56 +1,68 @@
-public class CheckDiagonals {
+import java.util.Scanner;
+
+public class sumOfDiagonalElements {
     public static void main(String[] args) {
         //9
-        int tab[][] = new int[4][4];
-        int tableLength = tab.length;
-        if (tableLength != tab[1].length){
-            System.out.println("Macie");
-        }
-        for (int i = 0; i < tab.length; i++) {
-            for (int j = 0; j < tab[i].length; j++) {
-                tab[i][j] = (int) (Math.random() * 10);
-                System.out.print(tab[i][j] + " ");
+        int rows = inputNumber("wierszy");
+        int columns = inputNumber("kolumn");
+        int tab[][] = new int[rows][columns];
+        if (rows <= 1 || columns <= 1 || rows != columns) {
+            System.out.println("Wykonanie operacji niemożliwe. Macierz nie posiada przekątnej głownej. Liczba wierszy(" + rows + ") rózna od liczby kolumn(" + columns + ").");
+        } else {
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < columns; j++) {
+                    tab[i][j] = (int) (Math.random() * 10);
+                    System.out.print(tab[i][j] + " ");
+                }
+                System.out.println();
             }
+
+            System.out.println("przekatna \\");
+
+            int sameValuesExist = 0;
+            for (int i = 0; i < rows; i++) {
+                for (int a = i + 1; a < columns; a++) {
+                    if (tab[i][i] == tab[a][a]) {
+                        System.out.println("tab[" + i + "][" + i + "]= " + tab[i][i] + " tab[" + a + "][" + a + "]= " + tab[a][a]);
+                        sameValuesExist++;
+                    }
+                }
+            }
+            if (sameValuesExist == 0) {
+                System.out.println("brak takich samych wartości w przekatnej \\");
+            }
+
             System.out.println();
-        }
+            System.out.println("przekatna /");
 
-        System.out.println("przekatna \\");
+            if (sameValuesExist != 0) {
+                sameValuesExist = 0;
+            }
 
-        int sameValuesExist = 0;
-        for (int i = 0; i < tableLength; i++) {
-            for (int a = i + 1; a < tableLength; a++) {
-                if (tab[i][i] == tab[a][a]) {
-                    System.out.println("tab[" + i + "][" + i + "]= " + tab[i][i] + " tab[" + a + "][" + a + "]= " + tab[a][a]);
-                    sameValuesExist++;
+            int lastElementIndex = rows - 1;
+
+            for (int i = 0; i < rows; i++) {
+                for (int a = i + 1; a < columns; a++) {
+                    int j = lastElementIndex - i;
+                    int b = lastElementIndex - a;
+                    if (tab[i][j] == tab[a][b]) {
+                        System.out.println("tab[" + i + "][" + (j) + "]= " + tab[i][j] + " tab[" + a + "][" + b + "]= " + tab[a][b]);
+                        sameValuesExist++;
+                    }
                 }
             }
-        }
-        if (sameValuesExist == 0) {
-            System.out.println("brak takich samych wartości w przekatnej \\");
-        }
 
-        System.out.println();
-        System.out.println("przekatna /");
-
-        if (sameValuesExist != 0) {
-            sameValuesExist = 0;
-        }
-
-        int lastElementIndex = tableLength - 1;
-
-        for (int i = 0; i < tableLength; i++) {
-            for (int a = i + 1; a < tableLength; a++) {
-                int j = lastElementIndex - i;
-                int b = lastElementIndex - a;
-                if (tab[i][j] == tab[a][b]) {
-                    System.out.println("tab[" + i + "][" + (j) + "]= " + tab[i][j] + " tab[" + a + "][" + b + "]= " + tab[a][b]);
-                    sameValuesExist++;
-                }
+            if (sameValuesExist == 0) {
+                System.out.println("brak takich samych wartości w przekatnej /");
             }
         }
+    }
 
-        if (sameValuesExist == 0) {
-            System.out.println("brak takich samych wartości w przekatnej /");
-        }
+    public static int inputNumber(String text) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Podaj liczbe " + text + ": ");
+        int number = in.nextInt();
+        return number;
     }
 }
